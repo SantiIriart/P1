96cd9a6c54bfa4a696e0134d93274c95f4565387
Santiago <santiagoiriar9@gmail.com>
2021-08-25 21:13:31 -0300

    Me estoy muriendo

---
+++ Prac5/Ej8.pas
@@ -0,0 +1,17 @@
+program Ej8;
+var letra, ant : Char;
+begin
+    Write('Ingrese una cadena de texto ');
+    Read(letra);
+    ant:=' ';
+    while letra <> '$' do
+        begin
+            if ant = letra then
+            begin
+              write(letra, ant, ' ');
+            end;
+             ant:=letra;
+            read(letra)
+        end;
+end.
+
