d8975e3515530db298ffb037cb14f6673c19490f
Santiago <santiagoiriar9@gmail.com>
2021-08-30 17:45:03 -0300

    Cualquier cosa

---
+++ Prac6/Ej2a.pas
@@ -0,0 +1,19 @@
+program Ej2;
+const cent = '.';
+var letra, oracion, ant : Char;
+cont : Integer;
+begin
+cont:=0;
+oracion:=' ';
+ant:=' ';
+Writeln('Ingrese una letra ');
+ReadLn(letra);
+WriteLn('Ingrese la oracion terminada en "."');
+repeat
+Read(oracion);
+if ((oracion=' ') or (oracion='.')) AND (letra=ant) then
+cont:=cont+1;
+ant:=oracion
+until oracion=cent;
+WriteLn(cont)
+end.
