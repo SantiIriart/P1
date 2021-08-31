f3c8081308c6d97867382a16921c6f2dc911e2ad
Santiago <santiagoiriar9@gmail.com>
2021-08-31 18:47:19 -0300

    nelson me trolleo

---
+++ Prac6/Ej3.pas
@@ -0,0 +1,11 @@
+program Ej3;
+const cent=-1;
+var
+x : real;
+coeficiente : integer;
+begin
+    Write('Ingrese un valor para x: ');
+    ReadLn(x);
+    Write('Ingrese los coeficientes naturales: ');
+    
+end.
