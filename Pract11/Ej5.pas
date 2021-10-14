a4262a5f3ff9cb6afba1cfe40496ea51f9346607
Santiago <santiagoiriar9@gmail.com>
2021-10-14 18:41:15 -0300

    14/10/2021

---
+++ Pract11/Ej5.pas
@@ -0,0 +1,19 @@
+program Ej5;
+{
+type
+    Err = (diverr, reserr, argerr);
+    TNerr = 
+    Nerr = record
+        valor : integer;
+        case tipo : TNerr
+
+    end;
+procedure division(a, b : Nerr; var resu : Nerr);
+begin
+    if b = 0 then
+        resu := 
+end;
+}
+begin
+  
+end.
